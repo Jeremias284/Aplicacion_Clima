6c2c90e2b037be5f029b6fe4b4d5e4d971c58a84
Jeremias Calzada <calzadajeremias246@gmail.com>
2021-07-12 17:40:21 -0300

    Actualizando Aplicacion Clima

---
+++ app.js
@@ -0,0 +1,12 @@
+//Elementos del DOM, para modificarlos posteriormente
+let searchForm = document.getElementById('search_submit');
+let searchInput = document.getElementById('search_input');  //valorInput
+let grados = document.getElementById('grados_Actual');
+let clima = document.getElementById('clima_Actual');
+let viento = document.getElementById('viento_Actual');
+let amanecer = document.getElementById('amanecer_Actual');
+let atardecer = document.getElementById('atardecer_Actual');
+let humedad = document.getElementById('humedad_Actual');
+let visibilidad = document.getElementById('visibilidad_Actual');
+let fecha = document.getElementById('fecha_Actual');
+
