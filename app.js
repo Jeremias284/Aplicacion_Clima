8e7a8c8ab4c48668b85c579f30e666cd4fb4b430
Jeremias Calzada <calzadajeremias246@gmail.com>
2021-07-12 20:24:29 -0300

    Actualizando Aplicacion Clima

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
-//Elementos del DOM, para modificarlos posteriormente
-let searchForm = document.getElementById('search_submit');
-let searchInput = document.getElementById('search_input');  //valorInput
-let grados = document.getElementById('grados_Actual');
-let clima = document.getElementById('clima_Actual');
-let viento = document.getElementById('viento_Actual');
-let amanecer = document.getElementById('amanecer_Actual');
-let atardecer = document.getElementById('atardecer_Actual');
-let humedad = document.getElementById('humedad_Actual');
-let visibilidad = document.getElementById('visibilidad_Actual');
-let fecha = document.getElementById('fecha_Actual');
+const api = {
+    key: '834c2ae38cefdca512c2bfc5629669b7',
+    url:`http://api.openweathermap.org/data/2.5/forecast`
+    // https://api.openweathermap.org/data/2.5/weather
+}
+
+var grados = document.querySelector('grados_Actual');
+var clima = document.querySelector('clima_Actual');
+var viento = document.querySelector('viento_Actual');
+var amanecer = document.querySelector('amanecer_Actual');
+var atardecer = document.querySelector('atardecer_Actual');
+var humedad = document.querySelector('humedad_Actual');
+var visibilidad = document.querySelector('visibilidad_Actual');
+var fecha = document.querySelector('fecha_Actual');
+
+
+async function search(objeto){
+    try{
+        const response = await fetch(`${api.url}?q=${objeto}&appid=${api.key}&lang=es`)
+        const data = await response.json();
+        
+
+        console.log(data);
+    }catch(err){
+        console.log(err);
+        alert('Hubo un error');
+    }
+}
+
+// function toCelsius(kelvin){
+//     return Math.round(kelvin - 273.15);
+// }
+
+function onSubmit(event){
+    event.preventDefault();
+    search(search_input.value);
+}
+const form = document.getElementById('search-form');
+const search_input = document.getElementById('search_input');
+form.addEventListener('submit', onSubmit, true);
 
